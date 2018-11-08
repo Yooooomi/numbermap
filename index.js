ec1e907ebdd5311bb6d7db1d8a8cc491dd719c0c
Timothee <timothee.boussus@gmail.com>
2018-11-08 12:01:13 +0100

    [+](Initial Commit): Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function applyNbMap() {
+    Number.prototype.map = function (cb) {
+        nbMap(this.valueOf(), cb);
+    }
+}
+
+const nbMap = (nb, cb) => {
+    let a = [];
+    for (let i = 0; i < nb; i++) {
+        a.push(cb(i));
+    }
+    return a;
+}
+
+module.exports = applyNbMap
